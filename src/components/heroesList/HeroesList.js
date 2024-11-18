f17e48b45d37b871c5b687ea9c56a5b810829084
Artem <artem.andreyev212@gmail.com>
2024-11-18 20:37:57 +0200

    Added redux thunk

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -5,7 +5,7 @@ import {useCallback} from "react";
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
 import { createSelector } from 'reselect';
 
-import { heroesFetching, heroesFetched, heroesFetchingError, heroDeleted, activeFilterChanged  } from '../../actions';
+import { fetchHeroes, heroDeleted } from '../../actions';
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
 
@@ -40,11 +40,7 @@ const HeroesList = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch("HEROES_FETCHING");
-        request("http://localhost:3001/heroes")
-            .then(data => dispatch(heroesFetched(data)))
-            .catch(() => dispatch(heroesFetchingError()))
-
+        dispatch(fetchHeroes(request));
         // eslint-disable-next-line
     }, []);
 
