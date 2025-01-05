dc7115fc00c8dad5291cc4268c83c3f9f8b167ab
Artem <artem.andreyev212@gmail.com>
2025-01-05 21:09:28 +0200

    Continue working with RTK Query, added delete endpoint with RTK, deleted heroesSlice because of RTK

--- src/components/heroesList/HeroesList.js
+++ src/components/heroesList/HeroesList.js
@@ -1,10 +1,8 @@
-import {useHttp} from '../../hooks/http.hook';
-import { useEffect, useCallback, useMemo } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useCallback, useMemo } from 'react';
+import { useSelector } from 'react-redux';
 import { CSSTransition, TransitionGroup} from 'react-transition-group';
 
-import { heroDeleted, fetchHeroes } from './heroesSlice';
-import { useGetHeroesQuery } from '../../api/apiSlice';
+import { useGetHeroesQuery, useDeleteHeroMutation } from '../../api/apiSlice';
 
 import HeroesListItem from "../heroesListItem/HeroesListItem";
 import Spinner from '../spinner/Spinner';
@@ -18,6 +16,8 @@ const HeroesList = () => {
         isLoading,
         isError
     } = useGetHeroesQuery();
+
+    const [deleteHero] = useDeleteHeroMutation();
 
     const activeFilter = useSelector(state => state.filters.activeFilter);
 
@@ -33,21 +33,15 @@ const HeroesList = () => {
 
     // const filteredHeroes = useSelector(filteredHeroesSelector);
     // const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
-    const dispatch = useDispatch();
-    const {request} = useHttp();
-
-    useEffect(() => {
-        dispatch(fetchHeroes());
-        // eslint-disable-next-line
-    }, []);
 
     const onDelete = useCallback((id) => {
-        request(`http://localhost:3001/heroes/${id}`, "DELETE")
-            .then(data => console.log(data, 'Deleted'))
-            .then(dispatch(heroDeleted(id)))
-            .catch(err => console.log(err));
+        deleteHero(id);
+        // request(`http://localhost:3001/heroes/${id}`, "DELETE")
+        //     .then(data => console.log(data, 'Deleted'))
+        //     .then(dispatch(heroDeleted(id)))
+        //     .catch(err => console.log(err));
         // eslint-disable-next-line  
-    }, [request]);
+    }, []);
 
     if (isLoading) {
         return <Spinner/>;
